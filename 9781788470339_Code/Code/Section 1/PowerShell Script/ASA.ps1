bf58800cb5824bf7b648e26b6da5a8225b53bed5
tenythomas01 <49268929+tenythomas01@users.noreply.github.com>
2019-05-21 15:12:12 +0530

    codes added

---
+++ 9781788470339_Code/Code/Section 1/PowerShell Script/ASA.ps1
@@ -0,0 +1,31 @@
+﻿ Login-AzureRmAccount
+
+  # Select the Azure subscription you want to use to create the resource group.
+ Get-AzureRmSubscription –SubscriptionName “Visual Studio Enterprise with MSDN” | Select-AzureRmSubscription
+
+ # If Stream Analytics has not been registered to the subscription, remove remark symbol below (#) to run the Register-AzureProvider cmdlet to register the provider namespace.
+ #Register-AzureRmResourceProvider -Force -ProviderNamespace 'Microsoft.StreamAnalytics'
+
+Get-AzureRMStreamAnalyticsJob
+
+#Get info about all Stream Analytics jobs in the subscription
+
+Get-AzureRMStreamAnalyticsJob -ResourceGroupName PacktDemos 
+
+#Lists all of the inputs are defined in a specified Stream Analytics job
+
+Get-AzureRMStreamAnalyticsJob -ResourceGroupName PacktDemos  -Name vehicletelemetry
+
+#Lists all of the inputs defined in a specified Get-AzureRMStreamAnalyticsJob -ResourceGroupName PacktDemos -Name StreamingJob
+
+Get-AzureRMStreamAnalyticsInput -ResourceGroupName PacktDemos -JobName vehicletelemetry -Name Input
+
+#This PowerShell command returns information about the outputs defined in the job vehicleTelemetry.
+
+Get-AzureRMStreamAnalyticsOutput -ResourceGroupName PacktDemos -JobName vehicletelemetry –Name Output
+
+#Gets information about a specific transformation defined in a Stream Analytics job.
+Get-AzureRMStreamAnalyticsTransformation -ResourceGroupName PacktDemos -JobName PAcktASADemo –Name Transformation
+#Start Stream Analytics job
+
+Start-AzureRMStreamAnalyticsJob -ResourceGroupName PacktDemos -Name packtasanetdemo -OutputStartMode CustomTime -OutputStartTime 2017-08-27T17:12:12Z
---
+++ 9781788470339_Code/Code/Section 1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Threading;
+using Microsoft.Azure;
+using Microsoft.Azure.Management.Insights;
+using Microsoft.Azure.Management.Insights.Models;
+using Microsoft.Azure.Management.StreamAnalytics;
+using Microsoft.Azure.Management.StreamAnalytics.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Azure.Insights;
+
+namespace ASAMonitoringJob
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string resourceGroupName = "<YOUR AZURE RESOURCE GROUP NAME>";
+            string streamAnalyticsJobName = "<YOUR STREAM ANALYTICS JOB NAME>";
+
+            // Get authentication token
+            TokenCloudCredentials aadTokenCredentials =
+                new TokenCloudCredentials(
+                    ConfigurationManager.AppSettings["SubscriptionId"],
+                    GetAuthorizationHeader());
+
+            Uri resourceManagerUri = new Uri(ConfigurationManager.AppSettings["ResourceManagerEndpoint"]);
+
+            // Create Stream Analytics and Insights management client
+            StreamAnalyticsManagementClient streamAnalyticsClient = new
+            StreamAnalyticsManagementClient(aadTokenCredentials, resourceManagerUri);
+            InsightsManagementClient insightsClient = new
+            InsightsManagementClient(aadTokenCredentials, resourceManagerUri);
+
+            // Get an existing Stream Analytics job
+            JobGetParameters jobGetParameters = new JobGetParameters()
+            {
+                PropertiesToExpand = "inputs,transformation,outputs"
+            };
+            JobGetResponse jobGetResponse = streamAnalyticsClient.StreamingJobs.Get(resourceGroupName, streamAnalyticsJobName, jobGetParameters);
+
+                      
+
+            // Enable monitoring
+            ServiceDiagnosticSettingsPutParameters insightPutParameters = new ServiceDiagnosticSettingsPutParameters()
+            {
+                Properties = new ServiceDiagnosticSettings()
+                {
+                    StorageAccountName = "<YOUR STORAGE ACCOUNT NAME>"
+                }
+            };
+            InsightsClient.ServiceDiagnosticSettingsOperations.Put(jobGetResponse.Job.Id, insightPutParameters);
+
+        }
+
+        
+        public static string GetAuthorizationHeader()
+
+            {
+            AuthenticationResult result = null;
+                var thread = new Thread(() =>
+                {
+                    try
+                    {
+                        var context = new AuthenticationContext(
+                            ConfigurationManager.AppSettings["ActiveDirectoryEndpoint"] +
+                            ConfigurationManager.AppSettings["ActiveDirectoryTenantId"]);
+
+                        result = context.AcquireToken(
+                            resource: ConfigurationManager.AppSettings["WindowsManagementUri"],
+                            clientId: ConfigurationManager.AppSettings["AsaClientId"],
+                            redirectUri: new Uri(ConfigurationManager.AppSettings["RedirectUri"]),
+                            promptBehavior: PromptBehavior.Always);
+                    }
+                    catch (Exception threadEx)
+                    {
+                        Console.WriteLine(threadEx.Message);
+                    }
+                });
+
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Name = "AcquireTokenThread";
+                thread.Start();
+                thread.Join();
+
+                if (result != null)
+                {
+                    return result.AccessToken;
+                }
+
+                throw new InvalidOperationException("Failed to acquire token");
+            
+        }
+        }
+    }
